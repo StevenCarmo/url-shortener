6facdc1ea114a37b18f1cdb19b6f4bbb64cc58bd
Steven Carmo <scarmo@intrenix.com>
2015-10-29 19:03:08 -0400

    setup basic ShortLink model and tests

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,5 +1,11 @@
 class ShortLink < ActiveRecord::Base
-  
+
+  LENGTH = 6
+
   belongs_to  :long_link, :inverse_of => :short_link
+
+  validates   :long_link_id, presence: true, uniqueness: true
+  validates   :slug, length: {maximum: LENGTH, minimum: LENGTH}
+  validates   :slug, presence: true, uniqueness: true
 
 end
--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -8,5 +8,31 @@ describe ShortLink, :type => :model do
     expect(subject).to be_valid
   end
 
+  describe "#create" do
+    it 'returns a ShortLink' do
+      expect(subject.class.to_s).to eq "ShortLink"
+    end
+
+    it "requires a long_link_id" do
+      expect(FactoryGirl.build(:short_link, :long_link_id => nil)).to be_invalid
+    end
+
+    it "requires a related LongLink record" do
+      @long_link = FactoryGirl.create(:long_link)
+      expect(FactoryGirl.create(:short_link, :long_link_id => @long_link.id)).to be_valid
+    end
+
+    it "slug is required" do
+      expect(FactoryGirl.build(:short_link, :slug => nil)).to be_invalid
+    end
+
+    it "length must be equal to LENGTH" do
+      subject { FactoryGirl.build(:short_link) }
+      subject.slug = "A" * ShortLink::LENGTH
+      
+      expect(subject).to be_valid
+    end
+   
+  end
   
 end
