e8cb1ff8cb752d1f94784fcd59b51b756fb9682a
Steven Carmo <scarmo@intrenix.com>
2015-10-29 18:10:42 -0400

    setup basic LongLink model and tests

--- app/models/long_link.rb
+++ app/models/long_link.rb
@@ -2,4 +2,17 @@ class LongLink < ActiveRecord::Base
 
   has_one :short_link, :inverse_of => :long_link
 
+  validate :must_be_valid_url
+  validates :url, presence: true, uniqueness: {case_sensitive: true , message: "Url is not unique"} 
+
+  def must_be_valid_url
+    errors.add(:base, 'Url is invalid') unless valid_url?
+  end
+
+  def valid_url?
+    # TODO tighten validation
+    url = URI.parse(self.url) rescue false
+    return url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)   
+  end
+
 end
--- spec/models/long_link_spec.rb
+++ spec/models/long_link_spec.rb
@@ -8,5 +8,34 @@ describe LongLink, :type => :model do
     expect(subject).to be_valid
   end
 
-  
+  describe "#create" do
+    it 'returns a LongLink' do
+      expect(subject.class.to_s).to eq "LongLink"
+    end
+
+    it "requires a valid url" do
+      expect(FactoryGirl.build(:long_link, :url => "htp://wwwo")).to be_invalid
+    end
+
+    it "requires a unique url" do
+      FactoryGirl.create(:long_link)
+      expect(FactoryGirl.build(:long_link)).to be_invalid
+    end    
+  end
+
+  describe "#valid_url?" do
+    subject { FactoryGirl.build(:long_link) }
+
+    it "returns false for invalid urls" do
+      subject.url = 'fdsafsa'
+      expect(subject.valid_url?).to eq false
+    end
+
+    it "returns true for valid urls" do
+      subject.url = 'http://www.google.com'
+      expect(subject.valid_url?).to eq true 
+    end
+
+  end
+
 end
